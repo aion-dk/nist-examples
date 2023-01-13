70856ea9bf756bb793acc08ae41fa8d612257f0b
Lukas Alex <lukas.alex@assemblyvoting.com>
2023-01-13 11:01:40 +0100

    Allow running validation script directly

--- validate.js
+++ validate.js
@@ -1,5 +1,7 @@
+#!/usr/local/bin/node
+
 const validate = require("jsonschema").validate;
-const fs = require('fs');
+const fs = require("fs");
 
 const schema = JSON.parse(fs.readFileSync("/usr/src/nist-v2.json"));
 const data = JSON.parse(fs.readFileSync("denver-general-2020/edf.json"));
