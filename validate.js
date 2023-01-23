8920a339f06781c53b3d8e3392ca4b0fdaab7456
Lukas Alex <lukas.alex@assemblyvoting.com>
2023-01-23 13:12:36 +0100

    Adding Famous Names election and validating all edfs

--- validate.js
+++ validate.js
@@ -3,7 +3,27 @@
 const validate = require("jsonschema").validate;
 const fs = require("fs");
 
-const schema = JSON.parse(fs.readFileSync("/usr/src/nist-v2.json"));
-const data = JSON.parse(fs.readFileSync("denver-general-2020/edf.json"));
+const getEDFs = (dirs, callback) => {
+  fs.readdir("/usr/src/app/", function(err, files) {
+    files.forEach((fileOrDir) => {
+      if (dirs.includes(fileOrDir)) {
+        const edf = JSON.parse(fs.readFileSync(`/usr/src/app/${fileOrDir}/edf.json`));
+        callback(fileOrDir, edf);
+      }
+    })
+  })
+}
 
-console.log(validate(data, schema));
+const schema = JSON.parse(fs.readFileSync("/usr/src/nist-v2.json"));
+
+const ELECTION_DIR = [
+  "denver-general-2020",
+  "famous-names"
+];
+
+getEDFs(ELECTION_DIR, (dirName, edf) => {
+  console.log(`============================================`)
+  console.log(`${dirName}/edf.json validating...`)
+  console.log(`============================================`)
+  console.log(validate(edf, schema));
+})
